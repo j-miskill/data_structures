5d715004d11f7d285feccd8f9e2d01b28958c235
j-miskill <jcmiskill@gmail.com>
2023-09-27 12:52:35 -0400

    Added another test for push to make sure strings can be added.

--- stack/test_stack.py
+++ stack/test_stack.py
@@ -1,14 +1,15 @@
 from stack import Stack
-import pytest 
+import pytest
 
-class TestStack():
+
+class TestStack:
 
     def test_push(self):
         s = Stack()
         s.push(1)
         s.push(2)
         assert s is not None
-        
+
     def test_top(self):
         s = Stack()
         s.push(1)
@@ -16,19 +17,27 @@ class TestStack():
         output = s.top()
         assert output == 2
         assert s.stack_size() == 2
-    
+
     def test_pop(self):
         s = Stack()
 
         s.push(1)
         s.push(2)
         output = s.pop()
-        
+
         assert output == 2
         assert s.stack_size() == 1
 
     def test_is_empty(self):
         s = Stack()
-        assert(s.is_empty() == True)
+        assert (s.is_empty() == True)
 
-        
+    def test_push_string(self):
+        s = Stack()
+        thrown = False
+        try:
+            s.push("Test String")
+        except:
+            thrown = True
+
+        assert (thrown == False)
